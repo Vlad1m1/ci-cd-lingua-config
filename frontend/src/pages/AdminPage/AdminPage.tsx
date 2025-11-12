ea54d8acddc3897e6fd2bd5577ab76a3d28a6f8b
Vlad1m1 <77707cx@gmail.com>
2025-11-13 02:56:18 +0300

    Убрал несуществующие панели в админке, мешает билду.

--- frontend/src/pages/AdminPage/AdminPage.tsx
+++ frontend/src/pages/AdminPage/AdminPage.tsx
@@ -1,22 +1,10 @@
 import LanguagePanel from "@components/AdminPanel/LanguagePanel";
-// @ts-expect-error
-import ModulePanel from "@components/AdminPanel/ModulePanel";
-// @ts-expect-error
-import LessonPanel from "@components/AdminPanel/LessonPanel";
-// @ts-expect-error
-import LevelPanel from "@components/AdminPanel/LevelPanel";
-// @ts-expect-error
-import QuestPanel from "@components/AdminPanel/QuestPanel";
 import TabbedContainer, { TabConfig } from "@components/TabbedContainer";
 
 
 const AdminPage = () => {
 	const tabConfig: TabConfig[] = [
 		{ id: "languages", label: "Язык", component: LanguagePanel },
-		{ id: "modules", label: "Модуль", component: ModulePanel },
-		{ id: "lessons", label: "Урок", component: LessonPanel },
-		{ id: "levels", label: "Уровень", component: LevelPanel },
-		{ id: "quests", label: "Квест", component: QuestPanel },
 	];
 
 	return <TabbedContainer config={tabConfig} />;
